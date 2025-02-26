503027d9e83bdfa618598cd7d2c5f03f20fa0214
UP240355 <up240 git config --global user.name Your>
2025-02-26 10:41:19 -0600

    1 to 2

--- Day 4.py
+++ Day 4.py
@@ -1,3 +1,7 @@
 # concatenate thirty days of python
 str= 'thirty'  + " "'days' " "+ 'of' " " + 'python'
 print(str )
+
+#concatenate coding for all
+str= 'coding' " "+ 'for' " "+ 'all' 
+print(str)
