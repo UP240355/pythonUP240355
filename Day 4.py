e395190b66855ee77ebd41ce0fbb163c4d41c1c1
UP240355 <up240 git config --global user.name Your>
2025-02-27 11:52:28 -0600

    3 to 9

--- Day 4.py
+++ Day 4.py
@@ -5,3 +5,35 @@ print(str )
 #concatenate coding for all
 str= 'coding' " "+ 'for' " "+ 'all' 
 print(str)
+
+# 3 and 4 declare variable and print company
+company= "coding for all"
+print(company)
+
+# length of company
+length_company=len('company')
+print(length_company)
+
+#change characters to upper
+min= ("thirty days of python")
+may= min.upper()
+print("Tu enunciado en mayusculas es:", may)
+
+#change characters to lower
+may= ("THIRTY DAYS OF PYTHON")
+min= may.lower()
+print("Tu enunciado en minusculas es:", min)
+
+# use methods
+str=("coding for all")
+str_capitalize= str.capitalize()
+str_title= str.title()
+str_swapcase= str.swapcase()
+print("the function capitalize is:", str_capitalize)
+print("the function title is:", str_title)
+print("the function swapcase is:", str_swapcase)
+
+# cut word
+str= ("coding for all")
+cut= str[6:14]
+print("now your sentence is: ", cut)
