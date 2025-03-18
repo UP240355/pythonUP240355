69a54523d5344d492b67c63c376c87eb92a6c1e8
UP240355 <up240 git config --global user.name Your>
2025-03-18 09:28:19 -0600

    avance

---
+++ 09_Day/Day9.py
@@ -0,0 +1,17 @@
+# drive
+print("level 1: ")
+print("programa 1")
+edad = int(input("enter your age:")   ) 
+if edad > 17 :
+    print(" you are old enough to dirve")
+else:
+    print(" you need wait for the missing amount of years")
+
+
+# compare values
+print("programa 2:")
+your_age= int(input("enter your age: "))
+if edad > your_age:
+    print(" tu edad es menor por: ",  edad - your_age, 'años' )
+else:
+    print(" tu edad es mayor por: ",  your_age - edad, 'años' )
