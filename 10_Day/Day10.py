3f4e4cfc90f77dfb6da2caf0f9b37c1e77c7566d
UP240355 <up240 git config --global user.name Your>
2025-03-26 10:41:23 -0600

    avance

---
+++ 10_Day/Day10.py
@@ -0,0 +1,14 @@
+# loop 0 to 10
+print("programa 1:")
+numbers=[0,1,2,3,4,5,6,7,8,9,10]
+for number in numbers:
+    print(number)
+
+#loop 10 to 0
+print("programa 2:")
+numbers=[10,9,8,7,6,5,4,3,2,1,0]
+for number in numbers:
+    print(number)
+
+# loop #
+
