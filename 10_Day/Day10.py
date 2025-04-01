fd02bd72a1f1aef6c00586c934cd3223e9f50eaa
UP240355 <up240 git config --global user.name Your>
2025-04-01 09:37:52 -0600

    avance

--- 10_Day/Day10.py
+++ 10_Day/Day10.py
@@ -1,14 +1,28 @@
 # loop 0 to 10
 print("programa 1:")
-numbers=[0,1,2,3,4,5,6,7,8,9,10]
+numbers=[0 , 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for number in numbers:
     print(number)
+
+# while
+count = 0
+while count <= 10:
+    print("bucle:" , count)
+count = count +  1
 
 #loop 10 to 0
 print("programa 2:")
-numbers=[10,9,8,7,6,5,4,3,2,1,0]
-for number in numbers:
+numeros=[10 , 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+for number in numeros:
     print(number)
+#while 2
+count = 10
+while count > -1:
+    print("while", count)
+count= count -1
 
 # loop #
-
+print("programa 3:")
+e ='#'
+for i in range (8):
+    print(e * 8)
