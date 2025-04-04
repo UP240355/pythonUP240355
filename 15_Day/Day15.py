e26ee6399aeb36d8609a207f9fd0f678e6d16c24
UP240355 <up240 git config --global user.name Your>
2025-04-03 19:20:29 -0600

    fiiin

---
+++ 15_Day/Day15.py
@@ -0,0 +1,19 @@
+#error
+print("programa 1:")
+
+print(python) #no esta definido 'python'
+
+hola= "coding for all"
+int(hola)   #no se puede castear un string a entero
+
+print("hello world"   #falta el parentesis de cierre
+      
+result= 'python' + 5 #no puedes sumar un string con un int
+
+list= [1, 2, 3]
+print(list[5]) # no hay un rango de 5 el cual tomar 
+
+dictionary = {"nombre": "yazid", "skills": "python" }
+print(dictionary["city"])  # no esta declarada city es inexistente
+
+div= 10 / 0  # el divisor no puede ser cero
